71f1ca3fe7b27ca11f33d99e61125263d78fe388
jessica <jessica.farias.rosado@gmail.com>
2021-01-14 09:11:37 -0600

    Adding prop-types

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types';
+
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+Items.propTypes = {
+  quantity: PropTypes.number.isRequired
+};
+
+Items.defaultProps = {
+  quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items quantity={10} />
+  }
+};
+
 
 class App extends React.Component {
     constructor(props) {
       super(props);
-      
     }
 
     render(){
-        return <div>Hello World</div>
+        return <div>Hello World 
+          <ShoppingCart/>
+        </div>
     }
 }
 
