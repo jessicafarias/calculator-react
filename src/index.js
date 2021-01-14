d17898b1b06c02991c2dc8a6f430b2a168102c97
jessica <jessica.farias.rosado@gmail.com>
2021-01-14 00:43:46 -0600

    Adding empty project
    Rendering div tag
    Creating App class inside index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      
+    }
+
+    render(){
+        return <div>Hello World</div>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
