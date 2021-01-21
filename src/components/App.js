8408babdea2ac6e6ff3efe3d0dbaf50005658bab
jessica.farias.rosado@gmail.com <jessica.farias.rosado@gmail.com>
2021-01-21 09:52:43 -0600

    Change App class into stateful component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends PureComponent {
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
   render() {
     return (
       <>
